e981713d7c2d0b8581471470e08490fb8f335d17
hxxyyyang <hxxyyyang@163.com>
2026-02-10 22:28:27 +0800

    update template

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
+export default defineConfig((options) => ({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
-  sourcemap: true,
-  target: 'node18'
-})
+  sourcemap: !!options.watch, 
+  target: 'node18',
+  splitting: false,
+  minify: !options.watch
+}))
